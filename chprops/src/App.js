d144749c3a1bee019e6a6801bdeed051a3567eb3
azazawejden <azazawejden@gmail.com>
2023-03-26 13:05:46 +0100

    first commit

--- chprops/src/App.js
+++ chprops/src/App.js
@@ -1,63 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
+import Profile from "./profile/Profile";
 
-const Profile = ({ fullName, bio, profession, children, handleName }) => {
+function App() {
   return (
-    <div style={styles.container}>
-      <div style={styles.image}>{children}</div>
-      <h1 style={styles.name} onClick={() => handleName(fullName)}>
-        {fullName}
-      </h1>
-      <h2 style={styles.profession}>{profession}</h2>
-      <p style={styles.bio}>{bio}</p>
+    <div className="App">
+      <Profile
+        fullName="wijden"
+        bio="student"
+        profession="master student"
+        handleName={(name) => alert(`Hello ${name}`)}
+      >
+        <img src="https://gomycodelearndev.blob.core.windows.net/profiles/bdd32436-2afe-431a-a990-66cdeb74bdb9-133195610347670294.jpg" alt="Profile" />
+      </Profile>
     </div>
   );
-};
+}
 
-Profile.propTypes = {
-  fullName: PropTypes.string.isRequired,
-  bio: PropTypes.string,
-  profession: PropTypes.string,
-  children: PropTypes.element.isRequired,
-  handleName: PropTypes.func.isRequired,
-};
-
-Profile.defaultProps = {
-  fullName: "Wijden Azaza",
-  bio: "",
-  profession: "Master student",
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "20px",
-    border: "1px solid black",
-  },
-  image: {
-    width: "200px",
-    height: "200px",
-    borderRadius: "50%",
-    overflow: "hidden",
-    marginBottom: "10px",
-  },
-  name: {
-    fontSize: "20px",
-    fontWeight: "bold",
-    cursor: "pointer",
-  },
-  profession: {
-    fontSize: "12px",
-    fontStyle: "italic",
-    marginBottom: "10px",
-  },
-  bio: {
-    fontSize: "12px",
-    textAlign: "center",
-  },
-};
-
-export default Profile;
+export default App;
+;
