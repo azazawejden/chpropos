6249f9ae5f28c420981a9c67baf7d19d4de6e5fb
azazawejden <azazawejden@gmail.com>
2023-03-26 12:44:41 +0100

    first commit

--- chprops/src/profile/Profile.js
+++ chprops/src/profile/Profile.js
@@ -1,32 +1,34 @@
+import React from "react";
 import PropTypes from "prop-types";
 
 const Profile = ({ fullName, bio, profession, children, handleName }) => {
-    return (
-        <div style={{ textAlign: "center" }}>
-            {children}
-            <h2>{fullName}</h2>
-            <p>{bio}</p>
-            <p>{profession}</p>
-            <button onClick={() => handleName(fullName)}>Show Name</button>
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      {children}
+      <h2>{fullName}</h2>
+      <h4>{profession}</h4>
+      <p>{bio}</p>
+      <button onClick={() => handleName(fullName)}>Click Me</button>
+    </div>
+  );
 };
 
 Profile.propTypes = {
-    fullName: PropTypes.string.isRequired,
-    bio: PropTypes.string.isRequired,
-    profession: PropTypes.string.isRequired,
-    children: PropTypes.element.isRequired,
-    handleName: PropTypes.func.isRequired,
+  fullName: PropTypes.string.isRequired,
+  bio: PropTypes.string.isRequired,
+  profession: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+  handleName: PropTypes.func.isRequired,
 };
 
 Profile.defaultProps = {
-    fullName: "Wijden Azaza",
-    bio: "No bio available",
-    profession: "Master student",
-    children: <img src="https://gomycodelearndev.blob.core.windows.net/profiles/bdd32436-2afe-431a-a990-66cdeb74bdb9-133195610347670294.jpg" alt="" />,
-    handleName: (name) => alert(`Hello, my name is ${wijden}`),
+  fullName: "John Doe",
+  bio: "No bio available",
+  profession: "Unknown",
+  children: <img src="default-image.png" alt="Profile" />,
+  handleName: (name) => alert(`Profile user's name is ${name}`),
 };
 
 export default Profile;
+
 
